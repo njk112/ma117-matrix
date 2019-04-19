43ee4f9aecfa787f0bd4b64c79c937efa167f5fb
Naglis Kazlauskas <naglis@TheBoss.local>
2019-04-19 17:38:31 +0300

    Initial Commit

---
+++ Project3.java
@@ -0,0 +1,38 @@
+/*
+ * PROJECT III: Project3.java
+ *
+ * This file contains a template for the class Project3. None of methods are
+ * implemented. Make sure you have carefully read the project formulation
+ * before starting to work on this file. You will also need to have completed
+ * the Matrix class, as well as GeneralMatrix and TriMatrix.
+ *
+ * Remember not to change the names, parameters or return types of any
+ * variables in this file!
+ *
+ * The function of the methods and instance variables are outlined in the
+ * comments directly above them.
+ */
+
+public class Project3 {
+    /**
+     * Calculates the variance of the distribution defined by the determinant
+     * of a random matrix. See the formulation for a detailed description.
+     *
+     * @param m           The matrix object that will be filled with random
+     *                    samples.
+     * @param numSamples  The number of samples to generate when calculating 
+     *                    the variance. 
+     * @return            The variance of the distribution.
+     */
+    public static double matVariance(Matrix m, int numSamples) {
+        // You need to fill in this method.
+    }
+    
+    /**
+     * This function should calculate the variances of matrices for matrices
+     * of size 2 <= n <= 50. See the formulation for more detail.
+     */
+    public static void main(String[] args) {
+        // You need to fill in this method.
+    }
+}
---
+++ TriMatrix.java
@@ -0,0 +1,128 @@
+/*
+ * PROJECT III: TriMatrix.java
+ *
+ * This file contains a template for the class TriMatrix. Not all methods are
+ * implemented. Make sure you have carefully read the project formulation
+ * before starting to work on this file. You will also need to have completed
+ * the Matrix class.
+ *
+ * Remember not to change the names, parameters or return types of any
+ * variables in this file!
+ *
+ * The function of the methods and instance variables are outlined in the
+ * comments directly above them.
+ */
+
+public class TriMatrix extends Matrix {
+    /**
+     * An array holding the diagonal elements of the matrix.
+     */
+    private double[] diag;
+
+    /**
+     * An array holding the upper-diagonal elements of the matrix.
+     */
+    private double[] upper;
+
+    /**
+     * An array holding the lower-diagonal elements of the matrix.
+     */
+    private double[] lower;
+    
+    /**
+     * Constructor function: should initialise m and n through the Matrix
+     * constructor and set up the data array.
+     *
+     * @param N  The dimension of the array.
+     */
+    public TriMatrix(int N) {
+        // You need to fill in this method.
+    }
+    
+    /**
+     * Getter function: return the (i,j)'th entry of the matrix.
+     *
+     * @param i  The location in the first co-ordinate.
+     * @param j  The location in the second co-ordinate.
+     * @return   The (i,j)'th entry of the matrix.
+     */
+    public double getIJ(int i, int j) {
+        // You need to fill in this method.
+    }
+    
+    /**
+     * Setter function: set the (i,j)'th entry of the data array.
+     *
+     * @param i    The location in the first co-ordinate.
+     * @param j    The location in the second co-ordinate.
+     * @param val  The value to set the (i,j)'th entry to.
+     */
+    public void setIJ(int i, int j, double val) {
+        // You need to fill in this method.
+    }
+    
+    /**
+     * Return the determinant of this matrix.
+     *
+     * @return The determinant of the matrix.
+     */
+    public double determinant() {
+        // You need to fill in this method.
+    }
+    
+    /**
+     * Returns the LU decomposition of this matrix. See the formulation for a
+     * more detailed description.
+     * 
+     * @return The LU decomposition of this matrix.
+     */
+    public TriMatrix decomp() {
+        // You need to fill in this method.
+    }
+
+    /**
+     * Add the matrix to another matrix A.
+     *
+     * @param A  The Matrix to add to this matrix.
+     * @return   The sum of this matrix with the matrix A.
+     */
+    public Matrix add(Matrix A){
+        // You need to fill in this method.
+    }
+    
+    /**
+     * Multiply the matrix by another matrix A. This is a _left_ product,
+     * i.e. if this matrix is called B then it calculates the product BA.
+     *
+     * @param A  The Matrix to multiply by.
+     * @return   The product of this matrix with the matrix A.
+     */
+    public Matrix multiply(Matrix A) {
+        // You need to fill in this method.
+    }
+    
+    /**
+     * Multiply the matrix by a scalar.
+     *
+     * @param a  The scalar to multiply the matrix by.
+     * @return   The product of this matrix with the scalar a.
+     */
+    public Matrix multiply(double a) {
+        // You need to fill in this method.
+    }
+
+    /**
+     * Populates the matrix with random numbers which are uniformly
+     * distributed between 0 and 1.
+     */
+    public void random() {
+        // You need to fill in this method.
+    }
+    
+    /*
+     * Your tester function should go here.
+     */
+    public static void main(String[] args) {
+        // You need to fill in this method.
+    }
+}
